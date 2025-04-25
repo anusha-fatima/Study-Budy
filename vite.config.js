fb9195153323c24b37ad46cb0f1de99807c34e89
unknown <anushafatima313@gmail.com>
2025-04-26 00:41:17 +0500

    Fix build issues

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', 
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        manualChunks: { 
+          react: ['react', 'react-dom'],
+          vendor: ['pdf-lib', 'mammoth']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
   optimizeDeps: {
     exclude: ['pdf-parse', 'mammoth'], 
-    include: ['pdf-lib'] 
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true, 
-      exclude: ['node_modules/pdf-parse/**'] 
-    }
+    include: ['react', 'react-dom'] 
   }
-})
+});
